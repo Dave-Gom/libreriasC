99f9b3183784e52bddafbdace730c07e11beeb74
Dave Gomez <davegomez426@gmail.com>
2022-04-06 00:00:51 -0400

    cadenas y caracteres

--- funciones.c
+++ funciones.c
@@ -15,7 +15,8 @@ int resta_errad(int n){
 }
 
 /* devuelve uno cero si todos los digitos del anio son diferentes
-caso contrario devuelce -1 */
+caso contrario devuelce -1*/
+/* funciona para anios de 4 dijitos pero se puede mejorar */
 int is_beautifull_year(int anio){
   int unid, dec, cent, uni_mil, ret = 0;
 
@@ -203,7 +204,7 @@ int sube_escalera( int n ){
 }
 
 /* genera un interador que carga valores en un arrego de dimension dada */
-void cargaVector( int arreglo[], int lenght){
+void cargaVector( int arreglo[], const int lenght){
   int iterador, valor;
 
   for(iterador = 0; iterador<lenght; iterador ++){
@@ -213,8 +214,8 @@ void cargaVector( int arreglo[], int lenght){
 
 }
 
-/* verifica si el valor actual y l pasado suman una cantidad dada */
-int agendador(int arreglo1[], int dim, int dias){
+/* verifica si el valor actual y el pasado suman una cantidad dada */
+int agendador(int arreglo1[], const int dim, const int dias){
 
   int persecutor = 0, adelantado, contador = 0;
   for( adelantado =1; adelantado<dim; adelantado++)
@@ -229,9 +230,150 @@ int agendador(int arreglo1[], int dim, int dias){
   return contador;
 }
 
-void copia_arreglo( int origen[], int destino[], int dim){
+void copia_arreglo( int origen[], int destino[], const int dim){
   int i;
   for( i= 0; i<dim; i++){
     destino[i]=origen[i];
   }
 }
+
+void cargaMatriz( const int fila, const int colum, int matriz[][colum] ){
+  int i,j, aux;
+  for(i=0; i<fila; i++){
+    for(j=0; j<colum; j++){
+      scanf("%d", &aux);
+      matriz[i][j]= aux;
+    }
+
+  }
+}
+
+void imprimeArreglo( int arreglo[], const int longitud){
+  int i;
+  for(i = 0; i<longitud; i++){
+    printf("%d", arreglo[i]);
+  }
+}
+
+/* separa los digitos de un entero en un arrayy */
+void digitosInt( int arreglo[], int valor){
+  int longitud = 9; /* un int solo puede almacenar numeros de hasta 9 digios */
+  int cont = 0, digito;
+  while(valor!=0)
+  {
+  digito=valor%10;
+  arreglo[cont] = digito;
+  valor=valor/10;
+  cont++;
+  }
+}
+
+/* inicia en cero todos los elementos de un arreglo */
+void iniciaArreglo( int arreglo[], const int lingitud){
+  int i;
+  for(i = 0; i<lingitud; i++){
+    arreglo[i] = 0;
+  }
+}
+
+void copiaPrimerafilaEnMatriz( const int fila, const int colum, int matriz[][colum] ){
+  int i,j, aux;
+  /* solicita que  si ingrese la primera fila del arreglo */
+  for (j = 0; j<colum; j++){  
+    scanf("%d", &aux);
+    matriz[0][j]= aux;
+  }
+
+  /* copia los valores de la primera fila en el resto de la matriz */
+  for(i=1; i<fila; i++){
+    for(j=0; j<colum; j++){
+      matriz[i][j] = matriz[0][j];
+    }
+  }
+}
+
+
+/* recibe un digito char del 0 al 9 y devuelve su valor entero */
+int charToInt( char caracter){
+  return caracter - '0';
+}
+
+/* entero a char */
+char enteroACaracter(int numero){
+    return numero + '0';
+}
+
+void quickSort( double array[], int inicio, int fin){
+  int i,j,central;
+  double pivote;
+  central = (inicio + fin)/2;
+  pivote = array[central];
+  double temporal;
+  do
+  {
+    while (array[i] < pivote) i++;
+    while (array[j] > pivote) j++;
+
+    if( i <= j){
+
+      temporal = array[i];
+      array[i] = array [j];
+      array[j] = temporal;
+      i++;
+      j--;
+
+    }
+    
+  } while (i<=j);
+
+  if( inicio <= j){
+    quickSort(array, inicio, j);
+  }
+
+  if( i< fin){
+    quickSort( array, i, fin);
+  }
+
+}
+
+/* imprime recursivamente los caracteres de una cadena en orden inverso */
+void inverso( const char * const ptrS )
+{
+  /* si es el final de la cadena */
+  if ( ptrS[ 0 ] == '\0' ) { /* caso base */
+    return; 
+  } /* fin de if */ 
+  else { /* si no es el final de la cadena */ 
+    inverso( &ptrS[ 1 ] ); /* paso recursivo */
+    putchar( ptrS[ 0 ] ); /* utiliza putchar para desplegar los caracteres */
+  } /* end else */
+} /* fin de la función inverso */
+
+/* lee una cadena con espacios */
+void leeConEspacios( char cadena[] ){
+  char c;
+  int i = 0;
+  while ( (c = getchar() ) != '\n' )
+  {
+    cadena[i++] = c;
+  } 
+  
+}
+
+
+/* separa las palabras de un enunciado en un array
+los argumentos son ( el array contenedor, el enunciado a dividir, el separador )
+ */
+void separaParabrasEnArray( char *array[], char enunciado[], char separador[]){
+
+  int i;
+  char *ptrToken = strtok(enunciado, separador);
+
+  while ( ptrToken != NULL)
+  {
+    array[i++] = ptrToken;
+    ptrToken = strtok( NULL, separador);
+  }
+
+}
+
