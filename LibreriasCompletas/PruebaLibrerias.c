227ed585031af3390570d31783042b1f113d0227
Dave Gomez <davegomez426@gmai.com>
2023-02-22 18:43:50 -0300

    Reorganizacion

--- LibreriasCompletas/PruebaLibrerias.c
+++ LibreriasCompletas/PruebaLibrerias.c
@@ -1,19 +1,48 @@
 #include "headerDocumentado.h"
 
+char *extraerColaListaString(ListaString *listaObjetivo);
 int main()
 {
-    ListaString ListaP = INICIALIZALISTA;
-    char exem[15];
-    cadenaAleatoria(exem, 15);
-    insertarEnColaListaString(&ListaP);
-    insertarEnColaListaString(&ListaP);
-    insertarEnColaListaString(&ListaP);
-    insertarEnColaListaString(&ListaP);
-    imprimeDesdeLaCabezaListaString(&ListaP);
-    char *aux = extraerCabezaListaString(&ListaP);
-    printf("%s\n", aux);
-    imprimeDesdeLaCabezaListaString(&ListaP);
-    imprimeDesdeLaColaListaString(&ListaP);
-
+    ListaString Prueba = {NULL, NULL, 0};
+    // int i;
+    char cadena[16];
+    for (int i = 0; i < 12; i++)
+    {
+        insertarStringEnCola(cadenaAleatoriaLongAleatoria(), &Prueba);
+    }
+    imprimeDesdeLaCabezaListaString(&Prueba);
+    extraerColaListaString(&Prueba);
+    imprimeDesdeLaCabezaListaString(&Prueba);
+    imprimeDesdeLaColaListaString(&Prueba);
     return 0;
+}
+
+/**
+ * @brief Extrae el ultimo elemento de la cola de la lista String
+ * @param listaObjetivo
+ * @return char*
+ */
+char *extraerColaListaString(ListaString *listaObjetivo)
+{
+    NodoString *ptrNodoObjetivo;
+    char *contenedor = malloc(sizeof(char *)); // contenedor Auxiliar
+    if (!listaStringEstaVacia(*listaObjetivo))
+    {
+        ptrNodoObjetivo = listaObjetivo->cola; // direccion de memoria del nodo a extraer
+
+        contenedor = ptrNodoObjetivo->dato;          // asigna el dato de la cola al registro contenedor
+        listaObjetivo->cola = ptrNodoObjetivo->ante; // asigna a la cola de la lista la direccion de memoria del nodo anterior a la cola extraida
+
+        listaObjetivo->cantidadElem--; // resta uno a la cantidad de elementos de la lista
+        if (listaObjetivo->cantidadElem == 0)
+            listaObjetivo->cabeza = NULL;
+        else
+            listaObjetivo->cola->sig = NULL; // elimina la direccion de memoria del apuntador siguiente de la cola actual
+
+        free(ptrNodoObjetivo); // libera el espacio en memoria ocupado por el nodo extraido
+    }
+    else
+        printf("\nLa lista esta vacia\n");
+
+    return contenedor;
 }
